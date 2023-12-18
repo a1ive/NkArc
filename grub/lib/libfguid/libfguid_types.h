4b52b9fbcbb87dc4ea2080cf022de53735d16d57
A1ive <10670106+a1ive@users.noreply.github.com>
2023-12-18 20:49:31 +0800

    libfguid

---
+++ grub/lib/libfguid/libfguid_types.h
@@ -0,0 +1,30 @@
+/*
+ * The internal type definitions
+ *
+ * Copyright (C) 2010-2022, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _LIBFGUID_INTERNAL_TYPES_H )
+#define _LIBFGUID_INTERNAL_TYPES_H
+
+#include <libyal_wrap.h>
+
+typedef intptr_t libfguid_identifier_t;
+
+#endif /* !defined( _LIBFGUID_INTERNAL_TYPES_H ) */
+
---
+++ grub/lib/libfguid/libfguid_unused.h
@@ -0,0 +1,30 @@
+/*
+ * Definitions to silence compiler warnings about unused function attributes/parameters.
+ *
+ * Copyright (C) 2010-2022, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _LIBFGUID_UNUSED_H )
+#define _LIBFGUID_UNUSED_H
+
+#define LIBFGUID_ATTRIBUTE_UNUSED
+
+#define LIBFGUID_UNREFERENCED_PARAMETER( parameter )
+
+#endif /* !defined( _LIBFGUID_UNUSED_H ) */
+
